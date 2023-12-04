d9a9e1e3396d36eb07ccfef43e6aeb1bd54b53d5
irfanpm <muhammmedirfanpm@gmail.com>
2023-12-04 17:33:44 +0530

    complete axioseInstance

--- redux/features/axioseInstance.jsx
+++ redux/features/axioseInstance.jsx
@@ -5,13 +5,13 @@ const admincookie=getCookie('admintoken')
 
  
   export const axiosInstance=axios.create({
-    baseURL:"http://127.0.0.1:8000",
+    baseURL:"https://spot-syncbackend.onrender.com",
     headers: {
       Authorization: `Bearer ${cookie}`,
     },
   })
   export const adminaxiosInstance=axios.create({
-    baseURL:"http://127.0.0.1:8000",
+    baseURL:"https://spot-syncbackend.onrender.com",
     headers: {
       Authorization: `Bearer ${admincookie}`,
     },
